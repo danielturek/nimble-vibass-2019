115e26540c4e6ff50b920f1d3e26e7eb5afe64ac
danielturek <danielturek@gmail.com>
2019-07-10 13:03:18 -0400

    added file R_setup.R

---
+++ R_setup.R
@@ -0,0 +1,8 @@
+setwd('~/github/nimble/nimble-vibass-2019/modules')
+library(methods)
+library(knitr)
+library(devtools)
+library(basicMCMCplots)
+library(coda)
+library(nimble)
+source('chunks_litters.R')
